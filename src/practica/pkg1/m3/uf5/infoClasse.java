0e2a7230bf026dd96875a318d0bf722cec6bb12d
davidblancolopez <daviddbl94@gmail.com>
2017-01-31 16:50:31 +0100

    CAMBIOS

--- src/practica/pkg1/m3/uf5/infoClasse.java
+++ src/practica/pkg1/m3/uf5/infoClasse.java
@@ -57,38 +57,6 @@ public class infoClasse <T>{
         return nomMetodes;
     }
     
-    // modificador d'un atribut.
-    public int[] modificadorAtribut(){
-        int[] modAtribut = new int[arrayMetodes().length];
-        Method[] meth = arrayMetodes();
-        for (int i = 0; i < modAtribut.length; i++) {
-            modAtribut[i] = meth[i].getModifiers();
-        }
-        return modAtribut;
-    }
-    
-    // modificador d'un mètode.
-    public int[] modificadorMetode() {
-        int[] modModificador = new int[arrayAtributs().length];
-        Field[] field = arrayAtributs();
-        for (int i = 0; i < modModificador.length; i++) {
-            modModificador[i] = field[i].getModifiers();
-        }
-        return modModificador;
-    }
-    
-    // modificar el valor d'un atribut.
-    public void modificarValorAtribut(Field atribut, Object nouValor) throws IllegalArgumentException, IllegalAccessException {
-        atribut.setAccessible(true);
-        atribut.set(t1, nouValor);
-    }
-    
-    // executar un dels seus mètodes.
-    public void executarMetodes(Method metode) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        metode.setAccessible(true);
-        metode.invoke(t1, null);
-    }
-    
     // crear una instància.
     public Object crearInstancia() throws InstantiationException, IllegalAccessException {
         return t1.getClass().newInstance();
