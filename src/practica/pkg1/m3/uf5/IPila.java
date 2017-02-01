a5febcfe6ac251ee0ec38b8cd41cc084d2297372
davidblancolopez <daviddbl94@gmail.com>
2017-02-01 20:13:49 +0100

    CAMBIOS

--- src/practica/pkg1/m3/uf5/IPila.java
+++ src/practica/pkg1/m3/uf5/IPila.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package practica.pkg1.m3.uf5;
 
-/**
- *
- * @author ALUMNEDAM
- */
-public interface IPila {
+import java.util.ArrayList;
+
+
+public interface IPila <T>{
+    
+    private ArrayList<T> llista;
+    
+    
+    public void afegir(T t) {
+        llista.add(t);
+    }
+
+    public T treure() {
+        T t = null;
+        if (!llista.isEmpty()) {
+            t = llista.remove(llista.size() - 1);
+        }
+        return t;
+    }
+    
+    
     
 }
---
+++ src/practica/pkg1/m3/uf5/PilaGenerica.java
@@ -0,0 +1,18 @@
+
+package practica.pkg1.m3.uf5;
+
+
+public class PilaGenerica {
+    
+    
+    
+    private class Node{
+        Node pare;
+        int valor;
+        
+        Node(Node pare, int valor){
+            this.pare = pare;
+            this.valor = valor;
+        }
+    }
+}
