e384f560fc12ad96d84f4bfa3efe37cd69ab52d1
davidblancolopez <daviddbl94@gmail.com>
2017-02-01 21:01:03 +0100

    CAMBIOS

--- src/practica/pkg1/m3/uf5/IPila.java
+++ src/practica/pkg1/m3/uf5/IPila.java
@@ -4,5 +4,13 @@ import java.util.ArrayList;
 
 public interface IPila<T> {
 
+    public void afegir(T t);
     
+    public T treure();
+    
+    public void buidar();
+    
+    public int mida();
+    
+    public void toArray();
 }
