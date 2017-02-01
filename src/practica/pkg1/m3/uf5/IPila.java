4391eca3cc26f6b5e9f2eb2d5b7b58f3c333fd4e
davidblancolopez <daviddbl94@gmail.com>
2017-02-01 20:20:04 +0100

    CAMBIOS

--- src/practica/pkg1/m3/uf5/IPila.java
+++ src/practica/pkg1/m3/uf5/IPila.java
@@ -6,20 +6,9 @@ import java.util.ArrayList;
 
 public interface IPila <T>{
     
-    private ArrayList<T> llista;
     
     
-    public void afegir(T t) {
-        llista.add(t);
-    }
-
-    public T treure() {
-        T t = null;
-        if (!llista.isEmpty()) {
-            t = llista.remove(llista.size() - 1);
-        }
-        return t;
-    }
+    
     
     
     
