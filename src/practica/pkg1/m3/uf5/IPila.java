80a5f01c22188671e6fc49e148fbbb234c3cc06d
davidblancolopez <daviddbl94@gmail.com>
2017-02-01 21:04:02 +0100

    CAMBIOS

--- src/practica/pkg1/m3/uf5/IPila.java
+++ src/practica/pkg1/m3/uf5/IPila.java
@@ -14,5 +14,31 @@ public interface IPila<T> {
     
     public void toArray();
     
+    /**
+     * private ArrayList <T> llista;
+
+    public Pila() {
+        llista = new ArrayList<>();
+    }
+
+    public void afegir(T t) {
+        llista.add(t);
+    }
+
+    public T treure() {
+        T t = null;
+        if (!llista.isEmpty()) {
+            t = llista.remove(llista.size() - 1);
+        }
+        return t;
+    }
+
+    public T[] arrayPila() {
+        return isEmpty()?null:(T[]) llista.toArray();
+    }
     
+    public boolean isEmpty(){
+        return llista.isEmpty();
+    }
+     */
 }
